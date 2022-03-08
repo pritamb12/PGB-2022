2bbfb5983b7aa00a16cf4a25efd615f7b8721201
localadmin <alimi.m@comakeit.com>
2022-03-08 15:05:46 +0530

    Added Day5 Task

---
+++ Manideep_PGB2022/java/ProjectGreen/src/Day5/task2/streams.java
@@ -0,0 +1,38 @@
+package Day5.task2;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.*;
+
+public class streams {
+	public static void main(String args[])
+	{
+		Integer a[]= {52,90,91,12,17,20,13,15,59,61};
+		List<Integer> l=Arrays.asList(a);
+		System.out.println("Converted Array into List\n"+l+"\n"+"Printing the prime numbers lessthan 25 in numbers");
+		
+		l.stream().filter(p->check(p) && p<25).forEach(System.out::println);
+		List<Integer> prime = l.stream().filter(p->check(p)).collect(Collectors.toList());
+		System.out.println("Printing the prime numbers in sorted order(Descending Order) ");
+		List<Integer> show =
+	            prime.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+		System.out.println(show);
+		System.out.println("Used Reduce function and the sum is :");
+		int add =
+			       l.stream().reduce(0,(ans,i)-> ans+i);
+		System.out.print(add);
+		
+	}
+
+	private static Boolean check(Integer p) {
+		int n=(int)p;
+		if(n<=1)return false;
+		for(int i=2;i*i<=n;i++)
+		{
+			if(n%i==0)
+				return false;
+		}
+		return true;
+		
+	}
+}
