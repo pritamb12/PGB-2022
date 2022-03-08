9f81185416950387da73e583ebb6f5cb9398aff3
ShyamCMIT <100761502+ShyamCMIT@users.noreply.github.com>
2022-03-08 17:47:54 +0530

    Update Problem.java

--- Shyam_PGB2022/Java/08-03-2022/Problem.java
+++ Shyam_PGB2022/Java/08-03-2022/Problem.java
@@ -26,23 +26,17 @@ public class Problem {
 		Supplier<Double> randomValue = () -> Math.random();// Creating Supplier
 		System.out.println(randomValue.get());
 		Integer a[]= {52,90,91,12,17,20,13,15,59,61};
-		List<Integer> l=Arrays.asList(a);
-		l.stream().map(n->n).forEach(System.out::println);
-		l.stream().filter(p->validate(p) && p<25).forEach(System.out::println);
-		List<Integer> prime = l.stream().filter(p->validate(p)).collect(Collectors.toList());
-		List<Integer> show = prime.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
-		System.out.println("The prime numbers in the input are:" +show);
-		int add =l.stream().reduce(0,(ans,i)-> ans+i);
-		System.out.println("Value after reduced is: "+add);	
+		List<Integer> list=Arrays.asList(a);
+		System.out.println("Elements: " + list);
+		System.out.println("Primes less than 25 in descending order: ");
+		list.stream().map(x -> isPrime(x)).filter(x -> x<=25).sorted(Comparator.reverseOrder()).forEach(x -> System.out.print(x+ " "));	
 	}
-	private static Boolean validate(Integer p) {
-		int n=(int)p;
-		if(n<=1)return false;
-		for(int i=2;i*i<=n;i++)
-		{
-			if(n%i==0)
-				return false;
-		}
-		return true;	
-	}
+	public static int isPrime(int number) {
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                return 99999;
+            }
+        }
+        return number;
+    }
 }
