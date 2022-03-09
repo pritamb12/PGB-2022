2a3b775900815e5399aa04a946f740bf8feec5a2
Praveen Mishra <praveen.mishra@comakeit.com>
2022-03-09 16:23:03 +0530

    Commit check

--- Sreeja_PGB2022/java/day5/Day_5/src/sreeja5/Task.java
+++ Sreeja_PGB2022/java/day5/Day_5/src/sreeja5/Task.java
@@ -4,23 +4,22 @@ import java.util.concurrent.TimeUnit;
 
 public class Task implements Runnable {
     private String name;
- 
+
     public Task(String name) {
         this.name = name;
     }
- 
+
     public String getName() {
         return name;
     }
- 
+
     public void run() {
         try {
             Long duration = (long) (Math.random() * 10);
             System.out.println("Executing : " + name);
             TimeUnit.SECONDS.sleep(duration);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
         }
     }
 }
-
