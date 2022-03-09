7998ce46cc07e563e22d42dca68407e09e028f4d
Kranthikumarjogu <kranthi.j@comakeit.com>
2022-03-09 16:27:30 +0530

    day1python

---
+++ kranthi_PGB-2022/PYTHON/day1/opondictionary.py
@@ -0,0 +1,15 @@
+dct={1:"hi",2:"kranthi",3:"welcome",4:"to python"}
+print(dct)
+print(" printing value of key:3 ")
+print(dct[3])
+print("removing the element with specified key: 1")
+dct.pop(1)
+print(dct)
+dct.popitem()
+print("removing the last inserted key value pair:")
+print(dct)
+t=1,2,3,
+dict1 = dict.fromkeys(t, 5)
+print("dictionay with same value:")
+print(dict1)
+
