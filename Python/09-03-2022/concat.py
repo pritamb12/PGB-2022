b8a19a5a1350f5e49c4181dd87e138b302393388
ShyamCMIT <100761502+ShyamCMIT@users.noreply.github.com>
2022-03-09 13:44:02 +0530

    Add files via upload

---
+++ Python/09-03-2022/concat.py
@@ -0,0 +1,14 @@
+Name = "Shyam sai Dogga"
+s = "Age is"
+x = 21
+b = True
+y = str(x)
+l = ["Shyam", "sai", "Dogga"]
+t = ("CMIT", 1079),  21, "Hyderabad"
+res = t + tuple(l)
+print(res)
+print(s + " " + y)
+print("{} {}".format(s, x))
+print(s + " " + str(b))
+print("{} {}".format(s, b))
+
