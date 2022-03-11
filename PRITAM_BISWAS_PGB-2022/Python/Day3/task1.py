a7e66e20da56a2f754114ad1d71a7e9febda7e97
pritamb12 <pritam.b@comakeit.com>
2022-03-11 16:28:17 +0530

    assignment

--- PRITAM_BISWAS_PGB-2022/Python/Day3/task1.py
+++ PRITAM_BISWAS_PGB-2022/Python/Day3/task1.py
@@ -271,48 +271,86 @@ print()
 	   ListBox should use a list, and DictBox should use a dict.
 """
 class Box:
-    def add(self, *items):
-        pass
     def empty(self):
-        pass
+        raise NotImplementedError()
 
     def count(self):
-        pass
+        raise NotImplementedError()
+
+    def add(self, *items):
+        raise NotImplementedError()
 
 
 class Item:
     def __init__(self, name, value):
-        self.name = name
         self.value = value
+        self.name = name
 
 
 class ListBox(Box):
     def __init__(self):
         self._items = []
 
-    def add(self, *i):
-        self._items.extend(i)
-
     def empty(self):
         items = self._items
         self._items = []
         return items
 
+    def displayItems(self):
+        if self.count() == 0:
+            print("No Items Found")
+            return
+        print("Item Attributes : ")
+        for i in self._items:
+            print(f"Name : {i.name}, Value : {i.value}")
+        return
+
     def count(self):
         return len(self._items)
+
+    def add(self, *items):
+        self._items.extend(items)
 
 
 class DictBox(Box):
     def __init__(self):
         self._items = {}
 
-    def add(self, *i):
-        self._items.update(dict((i.name, i) for i in i))
-
     def empty(self):
         items = list(self._items.values())
         self._items = {}
         return items
 
+    def displayItems(self):
+        if self.count() == 0:
+            print("No Items Found")
+            return
+        print("Item Attributes : ")
+        for k, v in self._items.items():
+            print(f"Name : {k}, Value : {v}")
+        return
+
     def count(self):
         return len(self._items)
+
+    def add(self, *items):
+        self._items.update(dict((i.name, i.value) for i in items))
+
+
+i1 = Item("Red", 1)
+i2 = Item("Blue", 2)
+i3 = Item("Green", 3)
+i4 = Item("Yellow", 4)
+i5 = Item("Black", 5)
+print("List Box\n")
+ObjListBox = ListBox()
+ObjListBox.add(i1, i2, i3)
+ObjListBox.displayItems()
+ObjListBox.empty()
+ObjListBox.displayItems()
+print("\n\nDictionary Box\n")
+ObjDictBox = DictBox()
+ObjDictBox.add(i4, i5)
+ObjDictBox.displayItems()
+ObjDictBox.empty()
+ObjDictBox.displayItems()
