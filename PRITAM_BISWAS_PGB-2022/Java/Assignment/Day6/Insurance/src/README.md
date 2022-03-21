771798a863d4a3ba333cc6875b17487249af3e9e
pritamb12 <pritam.b@comakeit.com>
2022-03-21 17:18:04 +0530

    assignment

--- PRITAM_BISWAS_PGB-2022/Java/Assignment/Day6/Insurance/src/README.md
+++ PRITAM_BISWAS_PGB-2022/Java/Assignment/Day6/Insurance/src/README.md
@@ -11,3 +11,26 @@ Output:
           Enter your choice : 1
           InsuranceType : Health Insurance
           Monthly Price : 196.00
+          
+          
+Association
+
+          [insurance@7e774085, insurance@3f8f9dd6] are insurance of the person Shubham Rastogi
+
+
+
+Aggregation
+
+           Policy Name: Abc Policy Number: 1234
+           State: hhhhh city: eee country :mmmmm
+
+
+
+
+composition
+          
+          
+          Title : EffectiveJ Java and  Author : Joshua Bloch
+          Title : Thinking in Java and  Author : Bruce Eckel
+          Title : Java: The Complete Reference and  Author : Herbert Schildt
+
--- PRITAM_BISWAS_PGB-2022/Java/Assignment/Day6/Insurance/src/aggregation.java
+++ PRITAM_BISWAS_PGB-2022/Java/Assignment/Day6/Insurance/src/aggregation.java
@@ -1,4 +1,59 @@
+class Addressclass{
+	String city;
+	String state;
+	String country;
+	
+	public Addressclass (String city,String state,String country)
+	{
+		this.city=city;
+		this.state=state;
+		this.country=country;
+		
+		
+	}
+}
+
 
 public class aggregation {
+	
+	int policy_no;
+	String Policy_name;
+	
+	Addressclass address;
+	
+	public aggregation(String Policy_name, int policy_no, Addressclass address) {
+		// TODO Auto-generated constructor stub
+		
+		this.Policy_name = Policy_name;
+		this.policy_no = policy_no;
+		this.address=address;
+	}
 
+	public int getPolicy_no() {
+		return policy_no;
+	}
+
+
+	public String getPolicy_name() {
+		return Policy_name;
+	}
+
+
+	
+	void display()
+	{
+		System.out.println("Policy Name: "+getPolicy_name()+"Policy Number: "+policy_no);
+		System.out.println("State: "+address.state+" city: "+address.city+" country :"+address.country);
+	}
+
+
+
+
+	public static void main (String[] args)
+	{
+	    Addressclass Addressobj=new Addressclass("eee","hhhhh","mmmmm");
+		aggregation obj = new aggregation("Abc ",1234,Addressobj);
+		obj.display();
+	}
+		
 }
